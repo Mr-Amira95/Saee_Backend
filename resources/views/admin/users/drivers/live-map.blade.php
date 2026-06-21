d8fbbdf30286d56cddcf9bc41db10121e5cf8142
hussam <hussam.amira95@gmail.com>
2026-06-21 23:46:47 +0300

    fix-live-map

--- resources/views/admin/users/drivers/live-map.blade.php
+++ resources/views/admin/users/drivers/live-map.blade.php
@@ -12,8 +12,7 @@
 @endsection
 
 @section('head')
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" crossorigin="">
 <style>
 #map {
     width: 100%; height: 580px;
@@ -152,8 +151,7 @@
 @endsection
 
 @section('scripts')
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV/XN2GQnY=" crossorigin=""></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" crossorigin=""></script>
 <script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
 @php
 $seedDrivers = $drivers->map(function ($d) {
--- resources/views/admin/users/drivers/location-history.blade.php
+++ resources/views/admin/users/drivers/location-history.blade.php
@@ -14,8 +14,7 @@
 @endsection
 
 @section('head')
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" crossorigin="">
 <style>
 #map {
     width: 100%; height: 520px;
@@ -210,8 +209,7 @@
 @endsection
 
 @section('scripts')
-<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV/XN2GQnY=" crossorigin=""></script>
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" crossorigin=""></script>
 @php
 $mapPoints = $points->map(function ($p) {
     return [
